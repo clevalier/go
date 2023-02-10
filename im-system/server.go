37eeaacfb6912ef13c9ddb792a269296d3b934b2
Cyrus <1652211045@qq.com>
2023-02-11 00:38:34 +0800

    (feat:service):即时通讯用户上线以及广播功能

--- im-system/server.go
+++ im-system/server.go
@@ -3,24 +3,40 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+	//在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+	//消息广播的channel
+	Message chan string
 }
 
-func NewSer(ip string, port int) *Server {
+func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
 func (this *Server) Handler(conn net.Conn) {
-	fmt.Println("连接建立成功")
-
+	//fmt.Println("连接建立成功")
+	user := NewUser(conn)
+	//用户上线，将用户加入到onlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	//广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+	//当前handler阻塞
+	select {}
 }
 
 func (this *Server) Start() {
@@ -32,6 +48,8 @@ func (this *Server) Start() {
 	}
 	//close listen socket
 	defer listen.Close()
+	//启动监听Message的goroutine
+	go this.ListenMessager()
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
@@ -39,7 +57,27 @@ func (this *Server) Start() {
 			continue
 		}
 		go this.Handler(conn)
+	}
+}
+
+//新增广播消息方法
+
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
+//新增监听广播消息channel方法,一旦有消息就发送给全部在线的user
+
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+		//将msg发给全部在线的user
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
 
 	}
-
 }
