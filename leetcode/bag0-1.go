37b81c5c793335b49d99748e335bdbd70e6bf545
Cyrus <1652211045@qq.com>
2023-02-13 11:36:37 +0800

    (fix:service):0-1背包问题

--- leetcode/bag0-1.go
+++ leetcode/bag0-1.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //有n件物品和一个最多能背重量为w 的背包。第i件物品的重量是weight[i]，得到的价值是value[i] 。每件物品只能用一次，求解将哪些物品装入背包里物品价值总和最大。
 func test_2_wei_bag_problem1(weight, value []int, bagweight int) int {
 	// 定义dp数组
@@ -18,14 +20,30 @@ func test_2_wei_bag_problem1(weight, value []int, bagweight int) int {
 			if j < weight[i] {
 				dp[i][j] = dp[i-1][j]
 			} else {
-				dp[i][j] = maxbag(dp[i-1][j], dp[i-1][j-weight[i]]+value[i])
+				dp[i][j] = maxBag(dp[i-1][j], dp[i-1][j-weight[i]]+value[i])
 			}
 		}
 	}
 	return dp[len(weight)-1][bagweight]
 }
 
-func maxbag(a, b int) int {
+func test_1_wei_bag_problem2(weight, value []int, bagWeight int) int {
+	// 定义 and 初始化：容量为j的背包所装的最大价值为dp[j]
+	//递推公式
+	dp := make([]int, bagWeight+1)
+	// 递推顺序
+	for i := 0; i < len(weight); i++ { //先物品
+		// 这里必须倒序,区别二维,因为二维dp保存了i的状态
+		for j := bagWeight; j >= weight[i]; j-- { //再背包重量
+			// 递推公式
+			dp[j] = maxBag(dp[j], dp[j-weight[i]]+value[i])
+		}
+	}
+	fmt.Println(dp)
+	return dp[bagWeight]
+}
+
+func maxBag(a, b int) int {
 	if a > b {
 		return a
 	}
@@ -35,5 +53,6 @@ func maxbag(a, b int) int {
 func main() {
 	weight := []int{1, 3, 4}
 	value := []int{15, 20, 30}
-	test_2_wei_bag_problem1(weight, value, 4)
+	a := test_1_wei_bag_problem2(weight, value, 4)
+	fmt.Print(a)
 }
