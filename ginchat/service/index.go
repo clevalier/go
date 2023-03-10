c91a00e189e97382825fe19f9eefe3e67bdf0377
Cyrus <1652211045@qq.com>
2023-03-10 16:23:08 +0800

    (feat:service):前后端分离以及swagger引入

--- ginchat/service/index.go
+++ ginchat/service/index.go
@@ -2,6 +2,10 @@ package service
 
 import "github.com/gin-gonic/gin"
 
+// GetIndex
+// @Tags         首页
+// @Success      200  {string}   welcome
+// @Router       /index [get]
 func GetIndex(context *gin.Context) {
 	context.JSON(200, gin.H{
 		"message": "welcome",
